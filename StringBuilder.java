b99eb9a555bb1630fae36801e9dd31ea5d5b63ac
Daniel Ostertag <k.d.ostertag@gmail.com>
2017-11-10 16:41:14 -0600

    Create StringBuilder.java

---
+++ StringBuilder.java
@@ -0,0 +1,25 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] t = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(build(t));
+    }
+
+    public static String build(int[] t) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("{");
+        for(int i = 0; i < t.length; i++) {
+            if(i % 4 == 0) {
+                builder.append("\n ");
+            }
+            builder.append(t[i]);
+            if(i != t.length - 1) {
+                builder.append(", ");
+            }
+        }
+        builder.append("\n}");
+        
+        return builder.toString();
+    }
+}
